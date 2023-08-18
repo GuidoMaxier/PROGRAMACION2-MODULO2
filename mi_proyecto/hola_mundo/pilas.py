4c321ef8c721714fbdd183b3301cf86b8d453a7a
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2023-08-18 18:28:21 -0300

    se agrega pila
    se agrega el archivo pila para luego editar

---
+++ mi_proyecto/hola_mundo/pilas.py
@@ -0,0 +1,71 @@
+class Stack:
+    def __init__(self):
+        self._data = []
+
+    #PUSH POP EMPTY TOP DISPLAY __LEN__
+    def  push(self, item):
+        self._data.append(item)   
+
+    def pop(self):
+        if not self.is_empty():
+            return self._data.pop()
+        else:
+            raise IndexError('La pila esta vacia')
+
+    def top(self):
+        if not self.is_empty():
+            return self._data[-1]
+        else:
+            raise IndexError('La pila esta vacia')
+        
+    def is_empty(self):
+        return len(self._data) == 0
+    
+    def __len__(self):
+        return len(self._data)
+    
+    def display(self):
+        print('La pila tiene los siguientes elementos')
+        return [print(i) for i in reversed(self._data)]
+    
+def balanceador(expresion): # Es un Balanceador
+    stack = Stack()
+    limiters = {')':'(', '}':'{', ']':'['}
+
+    for character in expresion:
+        if character in '([{':
+            stack.push(character)
+        elif character in ')]}':
+            if stack.is_empty() or stack.top() != limiters[character]:
+                return False
+            stack.pop()
+    return stack.is_empty()    
+
+
+expresion ="{(a+b) * (c + d)}"  
+expresion1 ="{(a+b) * (c + d)"  
+
+if balanceador(expresion1):
+    print('Los limitadores estan balanceados')
+else:
+    print('Los limitadores NO estan balanceados')  
+
+    
+
+pilaAAA= Stack()
+pilaAAA.push(10)
+pilaAAA.push(6)
+pilaAAA.push(7)
+
+pilaAAA.display()
+# print(len(pilaAAA))
+# print(pilaAAA.is_empty)
+print('----------------')
+print(pilaAAA.top())
+
+print(pilaAAA.pop())
+
+print('-------')
+pilaAAA.display()
+
+
